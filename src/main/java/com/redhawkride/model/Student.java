f4af4f1d2dbe67b8a49d0ec5824b6bc192b338ad
rmunie <rmunie1s@semo.edu>
2020-11-29 15:37:14 -0600

    made changes to student class and added the test with no test cases yet

--- src/main/java/com/redhawkride/model/Student.java
+++ src/main/java/com/redhawkride/model/Student.java
@@ -16,4 +16,88 @@ public class Student {
       riderStatus; // 0 - inactive, 1 - waiting for ride/driver available, 2 - ride in progress
   private AccountBalance accountBalance;
   private ArrayList<Trip> tripHistory;
+
+  String getStudentID(){
+    return studentID;
+  }
+
+  String getPassword(){
+    return password;
+  }
+
+  String getFirstName(){
+    return firstName;
+  }
+
+  String getLastName(){
+    return lastName;
+  }
+
+  String getPhoneNumber(){
+    return phoneNumber;
+  }
+
+  String getAddress(){
+    return address;
+  }
+
+  String getBankAccountNumber(){
+    return bankAccountNumber;
+  }
+
+  String getBankRoutingNumber(){
+    return bankRoutingNumber;
+  }
+
+  int getDriverStatus(){
+    return driverStatus;
+  }
+
+  ArrayList<Trip> gettripHistory(){
+    return tripHistory;
+  }
+
+  public void setStudentID(String ID){
+    this.studentID = ID;
+  }
+
+  public void setPassword(String key){
+    this.password = key;
+  }
+
+  public void setFirstName(String fname){
+    this.firstName = fname;
+  }
+
+  public void setLastName(String lname){
+    this.lastName = lname;
+  }
+
+  public void setPhoneNumber(String pNumber){
+    this.phoneNumber = pNumber;
+  }
+
+  public void setAddress(String addy){
+    this.address = addy;
+  }
+
+  public void setBankAccountNumber(String actNumber){
+    this.bankAccountNumber = actNumber;
+  }
+
+  public void setBankRoutingNumber(String routNumber){
+    this.bankRoutingNumber = routNumber;
+  }
+
+  public void setRideHistory(ArrayList<Trip> historyList){
+    this.tripHistory = historyList;
+  }
+
+  public void setIsAvailable(int avaibleFlag){
+    this.driverStatus = avaibleFlag;
+  }
+
+  public void addTrip(Trip x){
+    tripHistory.add(x); //insert trip into trip history array
+  }
 }
---
+++ src/test/java/com/redhawkride/StudentTest.java
@@ -0,0 +1,8 @@
+package com.redhawkride;
+
+import org.junit.jupiter.api.Test;
+
+public class StudentTest {
+    @Test
+    void
+}
